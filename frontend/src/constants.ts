b961a7fedff268defc41e9c16aa786b48ec7bab2
kash-patel <ksp.sdd.ptl@gmail.com>
2023-08-02 19:23:04 -0400

    Fill constants list.

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,5 +1,26 @@
+const BASE_URL =
+	process.env.NODE_ENV === "development" ? "http://localhost:3000" : "";
+const DEPARTMENTS_URL = "/api/departments";
+const ROLES_URL = "/api/roles";
 const USERS_URL = "/api/users";
+const LOCATIONS_URL = "/api/locations";
+const SYSTEMS_URL = "/api/systems";
+const SPECIES_URL = "/api/species";
+const ANIMALS_URL = "/api/animals";
+const VEHICLES_URL = "/api/vehicles";
+const ISSUES_URL = "/api/issues";
 
-const Constants = { USERS_URL };
+const Constants = {
+	BASE_URL,
+	DEPARTMENTS_URL,
+	ROLES_URL,
+	USERS_URL,
+	LOCATIONS_URL,
+	SYSTEMS_URL,
+	SPECIES_URL,
+	ANIMALS_URL,
+	VEHICLES_URL,
+	ISSUES_URL,
+};
 
 export default Constants;
