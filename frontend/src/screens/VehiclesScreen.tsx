5d0bf0587058dbf1d2b6bed712e856a66ae90099
kash-patel <ksp.sdd.ptl@gmail.com>
2023-08-06 16:53:12 -0400

    Add simple styling add delete buttons to vehicles table

--- frontend/src/screens/VehiclesScreen.tsx
+++ frontend/src/screens/VehiclesScreen.tsx
@@ -1,4 +1,5 @@
 import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 import {
 	useGetVehiclesQuery,
 	useDeleteVehicleMutation,
@@ -7,6 +8,18 @@ import BlockingLoader from "../components/BlockingLoader";
 
 const VehiclesScreen = () => {
 	const getVehiclesQuery = useGetVehiclesQuery(null);
+	const [deleteVehicle] = useDeleteVehicleMutation();
+
+	const handleDeleteClick = (id: number) => {
+		return async () => {
+			try {
+				await console.log(`Delete vehicle ${id}.`);
+				// await deleteVehicle(id);
+			} catch (error) {
+				throw error;
+			}
+		};
+	};
 
 	return (
 		<section>
@@ -24,13 +37,32 @@ const VehiclesScreen = () => {
 						getVehiclesQuery.error}
 				</p>
 			) : (
-				<ul>
-					{Object.values(getVehiclesQuery.data).map((v: any) => (
-						<li key={v.id}>
-							{v.make} {v.model}
-						</li>
-					))}
-				</ul>
+				<table className="mx-auto">
+					<thead>
+						<tr>
+							<th className="px-2 py-1">ID</th>
+							<th className="px-2 py-1">Make</th>
+							<th className="px-2 py-1">Model</th>
+							<th className="px-2 py-1"></th>
+						</tr>
+					</thead>
+					<tbody>
+						{Object.values(getVehiclesQuery.data).map((v: any) => (
+							<tr key={v.id}>
+								<td className="px-2 py-1">{v.id}</td>
+								<td className="px-2 py-1">{v.make}</td>
+								<td className="px-2 py-1">{v.model}</td>
+								<td className="px-2 py-1">
+									<Link onClick={handleDeleteClick(v.id)} to={"#"}>
+										<p className="bg-red-500 px-2 py-1 rounded-md text-white font-semibold">
+											Delete
+										</p>
+									</Link>
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
 			)}
 		</section>
 	);
