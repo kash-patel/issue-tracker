45da89fbd98f82b6b0f7a457873daed3ea167776
kash-patel <ksp.sdd.ptl@gmail.com>
2023-08-15 16:05:46 -0400

    Update HTTP-only cookie properties.

--- backend/src/utils/generateJWT.ts
+++ backend/src/utils/generateJWT.ts
@@ -9,7 +9,7 @@ const generateJWT = (res: any, userDetails: UserDetails) => {
 	res.cookie("jwt", token, {
 		httpOnly: true,
 		secure: process.env.NODE_ENV === "production" ? true : false,
-		sameSite: "strict",
+		sameSite: "none",
 		maxAge: 30 * 24 * 60 * 60 * 1000,
 	});
 };
