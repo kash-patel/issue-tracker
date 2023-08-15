ec5855156ea4d5264588d3b75bd9b1d1f43c1f1e
kash-patel <ksp.sdd.ptl@gmail.com>
2023-08-15 16:36:19 -0400

    Code cleanup test.

--- frontend/src/slices/apiSlice.ts
+++ frontend/src/slices/apiSlice.ts
@@ -3,16 +3,16 @@ import Constants from "../constants";
 
 const baseQuery = fetchBaseQuery({
 	baseUrl: Constants.BASE_URL,
-	credentials: "include",
-	prepareHeaders: (headers) => {
-		const token = localStorage.getItem("userDetails");
-		if (token) {
-			headers.set("Authorization", `Bearer ${token}`);
-			headers.set("Content-type", "application/json");
-		}
+	// credentials: "include",
+	// prepareHeaders: (headers) => {
+	// 	const token = localStorage.getItem("userDetails");
+	// 	if (token) {
+	// 		headers.set("Authorization", `Bearer ${token}`);
+	// 		headers.set("Content-type", "application/json");
+	// 	}
 
-		return headers;
-	},
+	// 	return headers;
+	// },
 });
 
 export const apiSlice = createApi({
